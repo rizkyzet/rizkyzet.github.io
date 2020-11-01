34cfce8221e28f43d2cbf9425d828c46d9b496eb
rizkyzet <rizkyzetzet121@gmail.com>
2020-11-01 18:19:31 +0700

    interact.js

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
-$('body').mousemove(function(e){
-    var amountMovedX = (e.pageX * -1 /50);
-    var amountMovedY = (e.pageY * -1 /50);
-    var amountMovedXp = (e.pageX * 1 /50);
-    var amountMovedYp = (e.pageY * 1 /50);
-    var x = (e.pageX * -1 /68);
-    var y = (e.pageY * -1 /68);
-    console.log('Y'+amountMovedY);
-    console.log('X'+amountMovedX);
+// $('body').mousemove(function(e){
+//     var amountMovedX = (e.pageX * -1 /50);
+//     var amountMovedY = (e.pageY * -1 /50);
+//     var amountMovedXp = (e.pageX * 1 /50);
+//     var amountMovedYp = (e.pageY * 1 /50);
+//     var x = (e.pageX * -1 /68);
+//     var y = (e.pageY * -1 /68);
+
    
-    $('.rectangle, .ellipse').css({
-        "transform": "translate(" + amountMovedX + "px," + amountMovedY + "px)"
-      });
-    $('.polygon').css({
-        "transform": "translate(" + amountMovedXp + "px," + amountMovedYp + "px)"
-      });
+//     $('.rectangle, .ellipse').css({
+//         "transform": "translate(" + amountMovedX + "px," + amountMovedY + "px)"
+//       });
+//     $('.polygon').css({
+//         "transform": "translate(" + amountMovedXp + "px," + amountMovedYp + "px)"
+//       });
   
    
 
-});
+// });
 
 $('.nav-link').click(function(){
   Swal.fire({
@@ -29,3 +28,25 @@ $('.nav-link').click(function(){
 
   })
 });
+
+
+const position = { x: 0, y: 0 }
+
+interact('.asako').draggable({
+  listeners: {
+    start (event) {
+      console.log(event.type, event.target)
+    },
+    move (event) {
+      position.x += event.dx
+      position.y += event.dy
+
+      event.target.style.transform =
+        `translate(${position.x}px, ${position.y}px)`
+    },
+  }
+});
+
+
+
+  
