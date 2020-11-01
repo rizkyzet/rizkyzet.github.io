8c96f1d0ccadcb29b1f0c3b67f8fa1f97c92eb88
rizkyzet <rizkyzetzet121@gmail.com>
2020-11-01 16:34:07 +0700

    commitku

---
+++ script.js
@@ -0,0 +1,24 @@
+$('body').mousemove(function(e){
+    var amountMovedX = (e.pageX * -1 /50);
+    var amountMovedY = (e.pageY * -1 /50);
+    var amountMovedXp = (e.pageX * 1 /50);
+    var amountMovedYp = (e.pageY * 1 /50);
+    var x = (e.pageX * -1 /68);
+    var y = (e.pageY * -1 /68);
+    console.log('Y'+amountMovedY);
+    console.log('X'+amountMovedX);
+    // $(this).css('rotateX','('+ amountMovedX +'deg)');
+    // $('.asako, .bg-rectangle, .ellipse-line, .polygon').css("transform", "rotateX("+y+"deg)" +" rotateY("+x+"deg)");
+   
+    $('.rectangle, .ellipse').css({
+        "transform": "translate(" + amountMovedX + "px," + amountMovedY + "px)"
+      });
+    $('.polygon').css({
+        "transform": "translate(" + amountMovedXp + "px," + amountMovedYp + "px)"
+      });
+  
+    // $(this).css("transform", "rotateY("+amountMovedX+"deg)");
+ 
+   
+
+});
