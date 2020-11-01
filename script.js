48648af1e99c68f8d12549dfe1e60e4036973df2
rizkyzet <rizkyzetzet121@gmail.com>
2020-11-01 20:46:53 +0700

    add icon and fix js

--- script.js
+++ script.js
@@ -1,68 +1,34 @@
-// $('body').mousemove(function(e){
-//     var amountMovedX = (e.pageX * -1 /50);
-//     var amountMovedY = (e.pageY * -1 /50);
-//     var amountMovedXp = (e.pageX * 1 /50);
-//     var amountMovedYp = (e.pageY * 1 /50);
-//     var x = (e.pageX * -1 /68);
-//     var y = (e.pageY * -1 /68);
+
+
+$('body').mousemove(function(e){
+    var amountMovedX = (e.pageX * -1 /50);
+    var amountMovedY = (e.pageY * -1 /50);
+    var amountMovedXp = (e.pageX * 1 /50);
+    var amountMovedYp = (e.pageY * 1 /50);
+
 
    
-//     $('.rectangle, .ellipse').css({
-//         "transform": "translate(" + amountMovedX + "px," + amountMovedY + "px)"
-//       });
-//     $('.polygon').css({
-//         "transform": "translate(" + amountMovedXp + "px," + amountMovedYp + "px)"
-//       });
+    $('.rectangle, .ellipse').css({
+        "transform": "translate(" + amountMovedX + "px," + amountMovedY + "px)"
+      });
+    $('.polygon').css({
+        "transform": "translate(" + amountMovedXp + "px," + amountMovedYp + "px)"
+      });
   
    
 
-// });
+});
 
 $('.nav-link').click(function(){
   Swal.fire({
     title: 'Oops!',
-    text: 'Web sedang dalam proses :)',
-  
+    text: 'Halaman sedang dalam proses pembuatan :)',
     imageUrl: 'img/download.jpeg',
     imageAlt: 'Custom image',
 
   })
 });
 
-
-const position = { x: 0, y: 0 }
-
-interact('.asako').draggable({
-  listeners: {
-    start (event) {
-      console.log(event.type, event.target)
-    },
-    move (event) {
-      position.x += event.dx
-      position.y += event.dy
-
-      event.target.style.transform =
-        `translate(${position.x}px, ${position.y}px)`
-    },
-  }
-});
-
-const positionPoly = { x: 0, y: 0 }
-
-interact('.polygon').draggable({
-  listeners: {
-    start (event) {
-      console.log(event.type, event.target)
-    },
-    move (event) {
-      positionPoly.x += event.dx
-      positionPoly.y += event.dy
-
-      event.target.style.transform =
-        `translate(${positionPoly.x}px, ${positionPoly.y}px)`
-    },
-  }
-});
 
 
 
