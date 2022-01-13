401f3702f89fd401b5c52ad81ac8ec244507feb0
rizkyzet <rizkyzetzet121@gmail.com>
2022-01-14 01:30:31 +0700

    init malem malem

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,29 @@
+const firstTyping = new TypeIt('.start-dialog-box', {
+        speed: 20,
+    }).type('<span class="p-0 m-0" style="font-size:12px;">HELLO, IM</span>')
+    .break()
+    .type('<span class="p-0 m-0 fs-2">MUHAMAD </span>')
+    .pause(500)
+    .move(-6)
+    .delete(1)
+    .pause(300)
+    .type('OC', {
+        speed: 400
+    })
+    .move(null, {
+        to: 'END'
+    })
+    .type('<span class="p-0 m-0 fs-2">RIZKI</span>')
+    .pause(300)
+    .delete(1)
+    .pause(300)
+    .type('<span class="p-0 m-0 fs-2">Y</span>')
+    .break()
+    .type('<span class="p-0 m-0" style="font-size:12px;">AN COMPUTER SCIENCE STUDENT WHO LOVE CODE & DESIGN</span>')
+
+
+firstTyping.go();
+
 const starterTyping = new TypeIt('.greeting-box', {
         speed: 20,
     }).type('<span class="p-0 m-0" style="font-size:12px;">HELLO, IM</span>')
@@ -26,8 +52,10 @@ const starterTyping = new TypeIt('.greeting-box', {
 
 const mainTyping = new TypeIt('.greeting-box', {
     speed: 10,
-}).type('<span class="p-0 m-0" style="font-size:16px;">Click to see details...</span>')
+}).type('<span class="p-0 m-0" style="font-size:16px;">i used to build a website using PHP, Javascript and CSS</span>')
 
+
+const startDialogContainer = document.querySelector('.start-dialog')
 
 
 let animeSkillPlay = () => {
@@ -35,7 +63,7 @@ let animeSkillPlay = () => {
         targets: ".box-skill",
         autoplay: false,
         opacity: 1,
-        scale:1,
+        scale: 1,
         right: {
             value: (el, i) => {
                 if (i == 0) {
@@ -70,7 +98,7 @@ let animeSkillBack = () => {
         targets: ".box-skill",
         autoplay: false,
         opacity: 1,
-        scale:0,
+        scale: 0,
         right: {
             value: (el, i) => {
                 if (i == 0) {
@@ -108,7 +136,9 @@ const navHome = document.querySelector('.nav-home');
 
 
 navSkills.addEventListener('click', function (e) {
-
+    if (startDialogContainer) {
+        startDialogContainer.remove()
+    }
     if (starterTyping.is('started')) {
         starterTyping.reset();
     }
@@ -118,6 +148,9 @@ navSkills.addEventListener('click', function (e) {
 
 
 navHome.addEventListener('click', function () {
+    if (startDialogContainer) {
+        startDialogContainer.remove()
+    }
     if (mainTyping.is('started')) {
         mainTyping.reset();
     }
